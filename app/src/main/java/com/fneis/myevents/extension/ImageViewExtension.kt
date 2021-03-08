537ea2074143e4438adbb49f04653615c91bbf6f
Filipe Amaral Neis <filipe.aneis@gmail.com>
2021-03-07 23:52:59 -0300

    Alteracão da lib de load de imagens

--- app/src/main/java/com/fneis/myevents/extension/ImageViewExtension.kt
+++ app/src/main/java/com/fneis/myevents/extension/ImageViewExtension.kt
@@ -1,30 +1,30 @@
 package com.fneis.myevents.extension
 
+import android.graphics.drawable.Drawable
 import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.fneis.myevents.R
-import com.squareup.picasso.Callback
-import com.squareup.picasso.Picasso
 
 fun ImageView.loadWith(url: String?, onSuccess: () -> Unit, onError: () -> Unit) {
 
-    url?.let { url ->
-        Picasso.get().load(url)
-            .fit()
-            .placeholder(R.drawable.progress_circle)
-            .centerCrop()
-            .into(
-                this,
-                object : Callback {
-                    override fun onSuccess() {
-                        onSuccess()
-                    }
+    Glide.with(this)
+        .load(url)
+        .centerCrop()
+        .placeholder(R.drawable.progress_circle)
+        .listener(object : RequestListener<Drawable> {
+            override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
+                onError()
+                return false
+            }
 
-                    override fun onError(e: Exception?) {
-                        onError()
-                    }
-                }
-            )
-    } ?: kotlin.run {
-        onError()
-    }
+            override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+                onSuccess()
+                return false
+            }
+        })
+        .into(this)
 }
